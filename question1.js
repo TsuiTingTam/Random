a0fa8a0ff722dda7dd6dd5a7560a4993dcf2fbcd
TsuiTingTam <tsuiting.tam@gmail.com>
2018-05-03 20:06:43 +1000

    1

--- question1.js
+++ question1.js
@@ -4,22 +4,24 @@ $(document).ready(function () {
         
         var pageNum = $(this).scrollTop()/$(this).height()+1;
         
+        //set back to random?
         if ( pageNum > 0 && pageNum < 2) {
             $('#white').text('ness');
             $('#green').text('random');
             $('#green').removeClass('random_light');
 
-            //add !
+            //remove -ness and add ?
         } else if ( pageNum > 1 && pageNum < 3) {
             $('#white').text('');
             $('#green').text('random?');
             $('#green').addClass('random_light');
-            //add !
+          
+            //change ? to !
         } else if ( pageNum > 2 && pageNum < 4){
             $('#white').text('');
             $('#green').text('random!');
             $('#green').addClass('random_light');
-            //set back to random?
+            
         }
         
     });
